dfa71d926244eec81f2f028cebc7f7b2b14a08f9
Старанцов Назар Евгеньевич <nazarstarantsov@gmail.com>
2020-05-01 18:12:51 +0300

    Finished

--- haskell/viginere.hs
+++ haskell/viginere.hs
@@ -1,7 +1,5 @@
 import Data.List
 
-main :: IO ()
-
 vigenere :: [Char] -> [Int] -> String -> String
 vigenere alphabet keys message = do
     zipWith shiftChar message keys
@@ -10,9 +8,9 @@ vigenere alphabet keys message = do
         shiftChar char key = if getIndexOfCharInAlphabet char /= -1 
             then ((cycle alphabet) !! (getPositiveIndex ((getIndexOfCharInAlphabet char) + key)))
             else char
-        getIndexOfCharInAlphabet char = [case elemIndex char alphabet of
+        getIndexOfCharInAlphabet char = case elemIndex char alphabet of
             Just n  -> n
-            Nothing -> -1]
+            Nothing -> -1
         getPositiveIndex index = if index > 0 then index else ((length alphabet) + index)
 
 decodeVigenere :: [Char] -> [Int] -> String -> String
@@ -20,7 +18,7 @@ decodeVigenere alphabet keys message = do
     let reversedSignKeys = zipWith (*) (replicate (length keys) (-1)) keys
     vigenere alphabet reversedSignKeys message
 
-
+main :: IO ()
 main = do 
     let result = vigenere ['a', 'b', 'c', 'd', 'e', 'f'] [-1, 5, 1, 2, 1, 1] "abcdfz"
     let decodedMessage = decodeVigenere ['a', 'b', 'c', 'd', 'e', 'f'] [-1, 5, 1, 2, 1, 1] result
