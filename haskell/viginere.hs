b37cab2725dae9ba8a3008c8aff46f143412053f
Старанцов Назар Евгеньевич <nazarstarantsov@gmail.com>
2020-05-01 17:45:47 +0300

    Added revering cypher

--- haskell/viginere.hs
+++ haskell/viginere.hs
@@ -10,12 +10,20 @@ vigenere alphabet keys message = do
         shiftChar char key = if getIndexOfCharInAlphabet char /= -1 
             then ((cycle alphabet) !! (getPositiveIndex ((getIndexOfCharInAlphabet char) + key)))
             else char
-        getIndexOfCharInAlphabet char = case elemIndex char alphabet of
+        getIndexOfCharInAlphabet char = [case elemIndex char alphabet of
             Just n  -> n
-            Nothing -> -1
+            Nothing -> -1]
         getPositiveIndex index = if index > 0 then index else ((length alphabet) + index)
+
+decodeVigenere :: [Char] -> [Int] -> String -> String
+decodeVigenere alphabet keys message = do
+    let reversedSignKeys = zipWith (*) (replicate (length keys) (-1)) keys
+    vigenere alphabet reversedSignKeys message
+
 
 main = do 
     let result = vigenere ['a', 'b', 'c', 'd', 'e', 'f'] [-1, 5, 1, 2, 1, 1] "abcdfz"
-    print (result)
+    let decodedMessage = decodeVigenere ['a', 'b', 'c', 'd', 'e', 'f'] [-1, 5, 1, 2, 1, 1] result
+    print ("Encoded to: " ++ result)
+    print ("Decoded to: " ++ decodedMessage)
 
